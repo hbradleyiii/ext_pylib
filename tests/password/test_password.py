a38f8e4b0cdc5d4d76aa6993f5d9f6c0ae08983d
Harold Bradley III <harold@bradleystudio.net>
2015-11-07 18:15:54 -0500

    Corrects a comment spelling error.

--- tests/password/test_password.py
+++ tests/password/test_password.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# name:             test_envi_file.py
+# name:             test_password.py
 # author:           Harold Bradley III
 # email:            harold@bradleystudio.net
 # created on:       11/07/2014
