e546d625d828479bcac1d81aa4e07bcdea89cc3a
Harold Bradley III <hbradley@mediamarketers.com>
2015-12-11 15:13:30 -0500

    Adds domain and files to import declaration.

--- __init__.py
+++ __init__.py
@@ -8,7 +8,9 @@ __author__ = 'Harold Bradley III'
 __license__ = 'GPL 2.0'
 __copyright__ = 'Copyright 2015 Harold Bradley III'
 
-__all__ = ['password', 'prompt']
+__all__ = ['domain', 'files', 'password', 'prompt']
 
+from . import domain
+from . import files
 from . import password
 from . import prompt
