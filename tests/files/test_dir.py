663810cb5e38cfb8cace32b66b11e2f0a23eeed6
Harold Bradley III <hbradleyiii@bradleystudio.net>
2015-12-17 09:46:03 -0500

    Adds tests for Dir class.

--- tests/files/test_dir.py
+++ tests/files/test_dir.py
@@ -10,12 +10,16 @@
 #
 
 from ext_pylib.files import Dir
+from mock import patch
 import pytest
 
 
 init_args = [
     (None, '<files.Dir:stub>'),
     ('/this/path', '/this/path/'),
+    ('/this//path/dir', '/this/path/dir/'),
+    ('/this//path//', '/this/path/'),
+    ('/this///path////', '/this/path/'),
 ]
 @pytest.mark.parametrize(("atts", "expected"), init_args)
 def test_dir_initialization(atts, expected):
@@ -23,14 +27,46 @@ def test_dir_initialization(atts, expected):
     dir = Dir({'path' : atts})
     assert str(dir) == expected
 
-def test_dir_create():
-    """TODO:"""
-    pass
+@patch('os.path.exists')
+@patch('os.makedirs')
+def test_dir_create(mock_makedirs, mock_exists):
+    """Test directory creation."""
+    mock_exists.return_value = False
+    dir = Dir({'path' : '/test/dir/'})
+    assert dir.create()
+    mock_makedirs.assert_called_once_with('/test/dir/')
 
-def test_dir_remove():
-    """TODO:"""
-    pass
+@patch('os.path.exists')
+@patch('os.makedirs')
+def test_dir_create_existing(mock_makedirs, mock_exists):
+    """Test creating dir when it already exists."""
+    mock_exists.return_value = True
+    dir = Dir({'path' : '/test/dir/'})
+    assert dir.create()
+    assert not mock_makedirs.called
+
+@patch('os.path.exists')
+@patch('shutil.rmtree')
+def test_dir_remove(mock_rmtree, mock_exists):
+    """Test directory removal."""
+    mock_exists.return_value = True
+    dir = Dir({'path' : '/test/dir/'})
+    assert dir.remove(False)
+    assert mock_rmtree.called_once_with('/test/dir/')
+
+@patch('os.path.exists')
+@patch('os.makedirs')
+def test_dir_remove_nonexisting(mock_rmtree, mock_exists):
+    """Test non-existing directory removal."""
+    mock_exists.return_value = False
+    dir = Dir({'path' : '/test/dir/'})
+    assert dir.remove(False)
+    assert not mock_rmtree.called
 
 def test_dir_fill():
+    """TODO:"""
+    pass
+
+def test_dir_actual_create_remove_and_fill():
     """TODO:"""
     pass
