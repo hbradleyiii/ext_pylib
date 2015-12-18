f4ea42c1d1b201c1bf1c8a983bdb989d97c0c7bc
Harold Bradley III <hbradleyiii@bradleystudio.net>
2015-12-17 20:13:21 -0500

    Adds file tests.

--- tests/files/test_file.py
+++ tests/files/test_file.py
@@ -10,23 +10,35 @@
 #
 
 from ext_pylib.files import File
+from mock import patch
 import pytest
 
 
 init_args = [
-    ({'path' : None},
-        {'path' : '<file.File:stub>', 'perms' : None, 'owner' : None, 'group' : None}),
+    (None, '<file.File:stub>'),
+    ('/this/file', '/this/file'),
+    ('/this//path//file', '/this/path/file'),
 ]
 @pytest.mark.parametrize(("atts", "expected"), init_args)
 def test_file_initialize(atts, expected):
-    """Test initialize Node."""
-    file = File(atts)
-    pass
+    """Test initialize File."""
+    file = File({'path' : atts})
+    assert str(file) == expected
+
+def test_file_initialize_with_trailing_slash():
+    """Test initializing a File with a trailing slash."""
+    with pytest.raises(ValueError):
+        file = File({'path' : '/this/path/'})
 
 def test_file_create():
     """TODO:"""
     pass
 
-def test_file_remove():
-    """TODO:"""
-    pass
+@patch('ext_pylib.files.node.Node.exists')
+@patch('os.remove')
+def test_file_remove(mock_remove, mock_exists):
+    """Test File remove method."""
+    mock_exists.return_value = True
+    file = File({'path' : '/test/dir/file'})
+    assert file.remove(False)
+    mock_remove.assert_called_once_with('/test/dir/file')
