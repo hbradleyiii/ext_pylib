ad0fe39108ecad49e8da9e364283771d3a6fd735
Harold Bradley III <hbradley@mediamarketers.com>
2016-01-07 06:38:09 -0500

    Updates comments for consistency.

--- tests/files/test_file.py
+++ tests/files/test_file.py
@@ -28,27 +28,27 @@ init_args = [
 ]
 @pytest.mark.parametrize(("atts", "expected"), init_args)
 def test_file_initialize(atts, expected):
-    """Test initialize File."""
+    """Tests initialize File."""
     file = File({'path' : atts})
     assert str(file) == expected
 
 def test_file_initialize_with_trailing_slash():
-    """Test initializing a File with a trailing slash."""
+    """Tests initializing a File with a trailing slash."""
     with pytest.raises(ValueError):
         file = File({'path' : '/this/path/'})
 
 def test_file_create():
-    """TODO: Test file creation."""
+    """TODO: Tests file creation."""
     pass
 
 def test_write_no_data():
-    """Test writing to a File with no data passed in."""
+    """Tests writing to a File with no data passed in."""
     file = File(DEFAULT_ARGS)
     with pytest.raises(UnboundLocalError):
         file.write()
 
 def test_write_data_with_handle():
-    """Test writing to a File with a handle passed in."""
+    """Tests writing to a File with a handle passed in."""
     class Mock_handle:
         """A Mock class for a handle."""
         data = None
@@ -89,20 +89,20 @@ def test_write_data_without_handle():
 
 @patch('ext_pylib.files.file.File.write')
 def test_file_append(mock_write):
-    """Test appending to file."""
+    """Tests appending to file."""
     File().append(None)
     mock_write.assert_called_once_with(None, True, None)
 
 @patch('ext_pylib.files.file.File.write')
 def test_file_overwrite(mock_write):
-    """Test overwriting a file."""
+    """Tests overwriting a file."""
     File().overwrite(None)
     mock_write.assert_called_once_with(None, False, None)
 
 @patch('ext_pylib.files.node.Node.exists')
 @patch('os.remove')
 def test_file_remove(mock_remove, mock_exists):
-    """Test File remove method."""
+    """Tests File remove method."""
     mock_exists.return_value = True
     file = File(DEFAULT_ARGS)
     assert file.remove(False)
