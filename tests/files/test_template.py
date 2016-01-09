41245437919c509e92dfc209c3c3edb5bc1921f1
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-01-09 16:56:44 -0500

    Adds template file class and template class stub.

---
+++ tests/files/test_template.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# name:             test_template.py
+# author:           Harold Bradley III
+# email:            harold@bradleystudio.net
+# created on:       01/09/2016
+#
+# description:      A unit test for ext_pylib file module's Template class and
+#                   methods.
+#
+
+from ext_pylib.files import TemplateFile
+from mock import patch
+import pytest
+
+
+def test_apply_using():
+    """TODO: Test apply_using() method."""
+    pass
