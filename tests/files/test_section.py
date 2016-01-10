c29f1a59a73e49cf04bed4acff751dda7961150f
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-01-10 06:36:34 -0500

    Adds a class SectionFile and the structure for testing this class.

---
+++ tests/files/test_section.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#
+# name:             test_section.py
+# author:           Harold Bradley III
+# email:            harold@bradleystudio.net
+# created on:       01/10/2016
+#
+# description:      A unit test for ext_pylib file module's Section class.
+#
+
+from ext_pylib.files import SectionFile
+from mock import patch
+import pytest
+
+
+def test_sectionfile_initialization():
+    """TODO: Test initialize SectionFile."""
+    pass
+
+def test_sectionfile_str():
+    """TODO: Test initialize SectionFile."""
+    pass
+
+def test_sectionfile_apply_to():
+    """TODO: Test initialize SectionFile."""
+    pass
