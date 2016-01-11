02303b2f56dc69bfc43342fdeda87a154d4d74b8
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-01-11 06:39:25 -0500

    Updates copyright to 2016.

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ __title__ = ''
 __version__ = '0.0.1'
 __author__ = 'Harold Bradley III'
 __license__ = 'GPL 2.0'
-__copyright__ = 'Copyright 2015 Harold Bradley III'
+__copyright__ = 'Copyright 2016 Harold Bradley III'
 
 __all__ = ['domain', 'files', 'password', 'prompt']
 
