64212ae3b0fa7f5822da0802866edc209de1043e
Harold Bradley III <hbradley@mediamarketers.com>
2016-01-21 12:29:31 -0500

    Adds tests for TemplateFile class.

--- tests/files/test_template.py
+++ tests/files/test_template.py
@@ -15,9 +15,35 @@ import pytest
 
 
 def test_templatefile_str():
-    """TODO: Test apply_using() method."""
-    pass
+    """Test SectionFile __str__ without path."""
+    file = TemplateFile()
+    assert str(file) == '<file.TemplateFile:stub>'
 
-def test_templatefile_apply_using():
-    """TODO: Test apply_using() method."""
-    pass
+TEMPLATE_FILE = """This is a test template file.
+#PLACEHOLDER#
+This is another #PLACEHOLDER#.
+Sample {#DATA#} sample.
+#DATA#, #DATA#, and #DATA#.
+But not #DATA, DATA#, and DATA.
+This is the last line.
+"""
+
+EXPECTED_RESULT = """This is a test template file.
+The placeholder text.
+This is another The placeholder text..
+Sample {www.google.com} sample.
+www.google.com, www.google.com, and www.google.com.
+But not #DATA, DATA#, and DATA.
+This is the last line.
+"""
+
+@patch('ext_pylib.files.file.File.read')
+def test_templatefile_apply_using(mock_read):
+    """Test SectionFile apply_using() method."""
+    file = TemplateFile()
+    mock_read.return_value = TEMPLATE_FILE
+    assert EXPECTED_RESULT == file.apply_using({
+        '#PLACEHOLDER#' : 'The placeholder text.',
+         '#DATA#' : 'www.google.com',
+         '#MISSING#' : 'Nothing here.',
+        })
