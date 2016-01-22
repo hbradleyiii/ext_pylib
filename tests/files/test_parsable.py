29233f084e9af8cdc3f5ac7a6a7dded659d64ca0
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-01-22 16:55:55 -0500

    Adds Parsable mixin functionality and tests.

--- tests/files/test_parsable.py
+++ tests/files/test_parsable.py
@@ -9,9 +9,34 @@
 #                   class.
 #
 
-from ext_pylib.files import Parsable
+from ext_pylib.files import File, Parsable
+import pytest
+
+
+FILE = """This is a sample file.
+This is a sample file.
+This is a sample file.
+DocumentRoot /var/www/google.com
+This is a sample file.
+"""
+
+# Monkey Patch function for read() method
+def read():
+    return FILE
+
+class ParsableFile(Parsable, File): pass
+
+
+def test_parsable_parse_with_existing_attribute():
+    """Test Parsable parse() method on an existing attribute."""
+    parsable = ParsableFile()
+    parsable.existing = 'already exists'
+    with pytest.raises(AttributeError):
+        parsable.parse({ 'existing' : '*' })
 
 def test_parsable_parse():
     """Test Parsable parse() method."""
     file = Parsable()
-    pass
+    file.read = read
+    file.parse({ 'htdocs' : 'DocumentRoot (.*)'})
+    assert file.htdocs[0] == '/var/www/google.com'
