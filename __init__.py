0d465c80fbbd5c9d7f182efbb9d253405fb31367
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-01-22 17:53:23 -0500

    Adds description to __init__

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 """
-TODO: Write description.
+ext_pylib - extra python libraries for scaffolding server scripts.
 """
 
-__title__ = ''
+__title__ = 'ext_pylib'
 __version__ = '0.0.1'
 __author__ = 'Harold Bradley III'
 __license__ = 'GPL 2.0'
