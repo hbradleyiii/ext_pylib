c896bcafe74b9f2aa2305aecfa48a3b01d8e6ef5
Harold Bradley III <hbradley@mediamarketers.com>
2016-01-23 12:50:56 -0500

    Updates copyright date.

--- __init__.py
+++ __init__.py
@@ -14,9 +14,8 @@ __title__ = 'ext_pylib'
 __version__ = '0.0.1'
 __author__ = 'Harold Bradley III'
 __license__ = 'GPL 2.0'
-__copyright__ = 'Copyright 2016 Harold Bradley III'
+__copyright__ = 'Copyright 2015-2016 Harold Bradley III'
 
-__all__ = ['domain', 'files', 'password', 'prompt']
 
 import domain
 import files
