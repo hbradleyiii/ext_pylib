c67e9ef8c346e4445d9aa2ef3ea91b7daa2cc108
Harold Bradley III <hbradley@mediamarketers.com>
2016-01-29 10:14:06 -0500

    Adds experimental meta module.
    In particular this adds a function for dynamically
    creating properties.

---
+++ meta/meta.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+#
+# name:             meta.py
+# author:           Harold Bradley III
+# email:            harold@bradleystudio.net
+# created on:       01/28/2016
+#
+# description:      Classes and functions for meta-programming with python.
+#                   This is mostly experimental stuff. For now, class
+#                   dynamic_property is merely a close reimplementation of
+#                   python's native property().
+#
+#                   Credit for much of this goes to:
+#                   http://eev.ee/blog/2012/05/23/python-faq-descriptors/
+#
+
+
+class dynamic_property(object):
+    """A re-implementation of python's native property().
+    most of this code from: http://eev.ee/blog/2012/05/23/python-faq-descriptors/"""
+
+    def __init__(self, getter=None):
+        self.getter = getter
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        return self.getter(instance)
+
+    def __set__(self, instance, value):
+        self.set_func(instance, value)
+
+    def setter(self, setter):
+        self.set_func = setter
+        return self
+
+    def set_func(self, instance, value):
+        raise TypeError("Cannot modify property.  It doesn't have a setter function.")
+
+def setdynattr(obj, attribute, getter_func=None, setter_func=None):
+    if not getter_func:
+        def getter_func(self):
+            return getattr(self, '_' + attribute, None)
+
+    if not setter_func:
+        def setter_func(self, value):
+            return setattr(self, '_' + attribute, value)
+
+    setattr(obj.__class__, attribute, dynamic_property(getter_func))
+    prop = getattr(obj.__class__, attribute)
+    prop = prop.setter(setter_func)
