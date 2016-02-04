8cacda5cc4805ad7c9ebb3d9e6174760f84e0ac3
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-02-04 10:08:40 -0500

    Updates docstrings.

--- prompt/prompt.py
+++ prompt/prompt.py
@@ -10,7 +10,7 @@
 #
 
 def prompt(prompt_text, default = True):
-    """Displays a yes/no prompt and returns a bool as the response."""
+    """Displays a yes/no prompt and returns the response as bool."""
     default_text = " [y] " if default else " [n] "
     while True:
         response = raw_input('[?] ' + prompt_text + default_text).lower()
@@ -27,7 +27,7 @@ def prompt(prompt_text, default = True):
             print 'Response not understood.'
 
 def prompt_str(prompt_text, default_str=''):
-    """Prompts for a string, returns the result"""
+    """Prompts for a string, returns the result."""
     add_text = " "
     if not default_str == '':
         add_text = " [" + default_str + "] "
@@ -38,7 +38,7 @@ def prompt_str(prompt_text, default_str=''):
     return response
 
 def warn_prompt(prompt_text, required_response):
-    """Warning prompt that prompts for a specific text.
+    """Warning prompt that prompts for a specific text (required_response).
     This is useful for potentially dangerous actions."""
     while True:
         response = raw_input("[!!] " + prompt_text + " [type n or '" + required_response + "'] ")
