c87fcbe83cad368967fec191d84b52776a88ae0d
Harold Bradley III <hbradley@mediamarketers.com>
2016-02-04 10:23:46 -0500

    Consolidates and cleans generate_pw().

--- password/password.py
+++ password/password.py
@@ -25,25 +25,15 @@ def generate_pw(length=18, char_set=DEFAULT_CHAR_SET):
     password = []
 
     while len(password) < length:
-        key = choice(char_set.keys())
-        a_char = urandom(1)
-        if a_char in char_set[key]:
-            if is_like_previous_char(password, char_set[key]):
-                continue
+        character = urandom(1)
+        subset = choice(char_set.keys())  # Get a random subset of characters
+                                          # from which to choose
+        # Make sure character is in subset
+        if character in char_set[subset]:
+            # Make sure it isn't the same subset as the previous character
+            if password and password[-1] in char_set[subset]:
+                continue  # Try again
             else:
-                password.append(a_char)
+                password.append(character)
+
     return ''.join(password)
-
-def is_like_previous_char(password, current_char_set):
-    """Function to ensure that there are no consecutive
-    UPPERCASE/lowercase/numbers/special-characters."""
-
-    num_of_chars = len(password)
-    if num_of_chars == 0:
-        return False
-    else:
-        prev_char = password[num_of_chars - 1]
-        if prev_char in current_char_set:
-            return True
-        else:
-            return False
