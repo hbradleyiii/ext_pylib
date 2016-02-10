9ad185e2d241e9a162eee195761a5ad7a03b5594
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-02-10 13:35:26 -0500

    Adds tests for error conditions in Section mixin.

--- tests/files/test_section.py
+++ tests/files/test_section.py
@@ -39,6 +39,21 @@ This string has the section.
 This string has the section.
 """
 
+FILE_BAD_SECTION_STR = """This string has the section.
+## END SECTION Test
+But in the wrong order...
+## START SECTION Test
+This string has the section.
+This string has the section.
+"""
+
+FILE_BAD_SECTION2_STR = """This string has the section.
+## START SECTION Test
+But in the no end section...
+This string has the section.
+This string has the section.
+"""
+
 FILE_WITHOUT_SECTION_STR = """This string does not have the section.
 This string does not have the section.
 This string does not have the section.
@@ -82,6 +97,15 @@ def test_section_is_in():
     assert section_file.is_in(FILE_WITH_SECTION_STR)
     assert not section_file.is_in(FILE_WITHOUT_SECTION_STR)
 
+def test_section_is_in_bad_data():
+    """Test Section is_in method passing in bad data."""
+    section_file = Section()
+    section_file.read = utils.mock_read(SECTION_STR)
+    with pytest.raises(ValueError):
+        assert section_file.is_in(FILE_BAD_SECTION_STR)
+    with pytest.raises(ValueError):
+        assert section_file.is_in(FILE_BAD_SECTION2_STR)
+
 def test_section_apply_to():
     """Test Section apply_to method."""
     section_file = Section()
@@ -99,6 +123,15 @@ def test_section_start_section_property():
     assert section_file.start_section == "This is the first line."
     section_file.read = utils.mock_read(MULTILINE_STR_WITH_RETURN)
     assert section_file.start_section == "This is the first line."
+
+def test_section_start_section_property_bad_section():
+    """Test Section start_section property with invalid section data."""
+    section_file = Section()
+    section_file.read = utils.mock_read('')
+    with pytest.raises(EOFError):
+        assert section_file.start_section
+    with pytest.raises(EOFError):
+        assert section_file.end_section
 
 def test_section_end_section_property():
     """Test Section end_section property."""
