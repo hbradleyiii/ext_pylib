8931fb55bf5848cc65000ba36b10b241cf325da5
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-02-26 14:41:39 -0500

    Updates to ansi and colors.
    Going a bit of a different direction, this will likely change a bit more until it becomes stable.

--- ext_pylib/terminal/ansi.py
+++ ext_pylib/terminal/ansi.py
@@ -11,36 +11,182 @@
 ext_pylib.terminal.ansi
 ~~~~~~~~~~~~~~~~~~~~~~~
 
-Ansi escape codes.
+Ansi escape codes and functions.
+
+Most of these functions write their results directly to standard out unless
+passed get_string=True, in which case they just return the escape as a string.
 """
 
+from sys import stdout
+
+
+# GENERAL ASCII CODES
+
+NULL = '\00' # Null character
+BEL = '\007' # Terminal Bell
+BS = '\010'  # Backspace
+HT = '\011'  # Horizontal Tab
+LF = '\012'  # Linefeed (newline)
+VT = '\013'  # Vertical Tab
+FF = '\014'  # Formfeed (or NP: new page)
+CR = '\015'  # Carriage Return
+ESC = '\033' # Escape character
+DEL = '\177' # Delete character
+
+
+# ESCAPE SEQUENCES
 
 CSI = '\033['
 OSC = '\033]'
-BEL = '\007'
+DELIMITER = ';'
 
-CUU = CSI + '{0}A'  # Cursor up
-CUD = CSI + '{0}B'  # Cursor down
-CUF = CSI + '{0}C'  # Cursor down
-CUB = CSI + '{0}D'  # Cursor down
-CNL = CSI + '{0}E'  # Cursor down
-CPL = CSI + '{0}F'  # Cursor down
-CHA = CSI + '{0}G'  # Cursor Horizontal Absolute
-CUP = CSI + '{0};{1}H'  # Cursor Horizontal Absolute
+def attributes(*args):
+    """Returns multiple attributes concatenated and separated by DELIMITER."""
+    return reduce(lambda a, b: str(a) + DELIMITER + str(b), args)
 
-ED = CSI + '{0}J'  # Erase part of the screen
+def escape(escape, get_string):
+    """Prints an escape sequence or returns it as a string."""
+    if get_string:
+        return escape
+    stdout.write(escape)
+    stdout.flush()
 
-SCP = CSI + 's'  # Save Cursor Position
-RCP = CSI + 'u'  # Restore Cursor Position
 
-SGR = CSI + '{0}m'
-SGR_RESET = CSI + '0m'
-SGR_SEP = ';'  # SGR separator (delimiter)
+    ## Cursor Handling ##
 
-ATT_RESET = '0'
+def cursor_hide(get_string=False):
+    """Hides the cursor."""
+    return escape(ansi.CSI + '?25l', get_string)
+
+def cursor_show(get_string=False):
+    """Shows the cursor."""
+    return escape(ansi.CSI + '?25h', get_string)
+
+def cursor_up(n=1, get_string=False):
+    """Moves your cursor up 'n' cells."""
+    return escape(CSI + str(n) + 'A', get_string)
+
+def cursor_down(n=1, get_string=False):
+    """Moves your cursor down 'n' cells."""
+    return escape(CSI + str(n) + 'B', get_string)
+
+def cursor_right(n=1, get_string=False):
+    """Moves your cursor right (forward) 'n' cells."""
+    return escape(CSI + str(n) + 'C', get_string)
+
+def cursor_left(n=1, get_string=False):
+    """Moves your cursor left (backward) 'n' cells."""
+    return escape(CSI + str(n) + 'D', get_string)
+
+def cursor_next_line(n=1, get_string=False):
+    """Moves your cursor to the next 'n' lines."""
+    return escape(CSI + str(n) + 'F', get_string)
+
+def cursor_previous_line(n=1, get_string=False):
+    """Moves your cursor to the previous 'n' lines."""
+    return escape(CSI + str(n) + 'G', get_string)
+
+def cursor_horizontal_absolute(n=1, get_string=False):
+    """Moves your cursor to the 'n' column."""
+    return escape(CSI + str(n) + 'G', get_string)
+
+def cursor_position(x=0, y=0, get_string=False):
+    """Moves youescape = cursor to position (x, y).
+    NOTE: This assumes starting at (0, 0) which is different than the ANSI
+    standard; it also assumes (x, y) and not (y, x) per ANSI standard."""
+    return escape(CSI + attributes(y+1, x+1), get_string)
+
+def cursor_save(get_string=False):
+    """Save the current cursor position."""
+    return escape(CSI + 's', get_string)
+
+def cursor_restore(get_string=False):
+    """Restore the last saved cursor position."""
+    return escape(CSI + 'u', get_string)
+
+def attributes_save(get_string=False):
+    """Save the current cursor position and attributes."""
+    return escape(ESC + '7', get_string)
+
+def attributes_restore(get_string=False):
+    """Restore the last saved cursor position and attributes."""
+    return escape(ESC + '8', get_string)
+
+def get_cursor_pos():
+    """Returns a tuple of (x, y) of current cursor position.
+    Note this follows conventional (x, y) order and starts with (0, 0) and not
+    the order according to the ANSI standard."""
+    QCU = CSI + '6n'  # Query cursor position (to stdin)
+    RCU = CSI + '{0};{1}R' # Reports Cursor positon (result from above query
+                           # Reports as <ESC>[{row};{column}R
+                           # Note that this is backwards order
+
+
+    ## Clearing the screen ##
+
+def clear(get_string=False):
+    """Clears the entire screen."""
+    return escape(CSI + '2J', get_string)
+
+def clear_down(get_string=False):
+    """Clears the screen from the cursor down."""
+    return escape(CSI + '0J', get_string)
+
+def clear_up(get_string=False):
+    """Clears the screen from the cursor down."""
+    return escape(CSI + '1J', get_string)
+
+def clear_line(get_string=False):
+    """Clears the entire line."""
+    return escape(CSI + '2K', get_string)
+
+def clear_line_forward(get_string=False):
+    """Clears the entire line."""
+    return escape(CSI + '0K', get_string)
+
+def clear_line_back(get_string=False):
+    """Clears the entire line."""
+    return escape(CSI + '1K', get_string)
+
+
+    ## Terminal Settings ##
+
+def reset_terminal(get_string=False):
+    return escape(ESC + 'c', get_string)
+
+def enable_line_wrap(get_string=False):
+    return escape(CSI + '7h', get_string)
+
+def disable_line_wrap(get_string=False):
+    return escape(CSI + '7h', get_string)
+
+def set_scroll_all(get_string=False):
+    """Enable scrolling for the entire screen."""
+    return escape(CSI + 'r', get_string)
+
+def set_scroll(start_row, end_row, get_string=False):
+    """Enable scrolling from start_row to end_row."""
+    return escape(CSI + attributes(start_row, end_row) + 'r', get_string)
+
+def scroll_up(get_string=False):
+    return escape(ESC + 'D', get_string)
+
+def scroll_down(get_string=False):
+    return escape(ESC + 'M', get_string)
+
+
+    ## SGR Escape Sequences ##
+
+def sgr(*args):
+    """Returns an SGR (Select Graphic Rendition) escape sequence given one or
+    more attributes."""
+    return CSI + attributes(*args) + 'm'
+
+RESET = '0'
 BOLD = '1'
 UNDERLINE = '4'
 BLINK = '5'
+RBLINK = '6'
 REVERSE = '7'
 CONCEAL = '8'
 
@@ -52,6 +198,7 @@ BLUE = '34'
 MAGENTA = '35'
 CYAN = '36'
 WHITE = '37'
+EXTENDED = '38'
 
 BG_BLACK = '40'
 BG_RED = '41'
@@ -61,3 +208,4 @@ BG_BLUE = '44'
 BG_MAGENTA = '45'
 BG_CYAN = '46'
 BG_WHITE = '47'
+BG_EXTENDED = '48'
